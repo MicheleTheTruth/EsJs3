78db5be526e6569ab87d35c0a83be0e69821df2c
MicheleTheTruth <verita.michele@studenti.salesianisesto.it>
2020-12-04 08:47:58 +0100

    calcolator working

--- Es3.js
+++ Es3.js
@@ -1,26 +1,25 @@
-$("btn+").on("click", calcola("+"));
-$("btn-").on("click", calcola("-"));
-$("btn/").on("click", calcola("/"));
-$("btn*").on("click", calcola("*"));
+$("#btn1").click(function () {calcola("+");});
+$("#btn2").click(function () {calcola("-");});
+$("#btn3").click(function () {calcola("/");});
+$("#btn4").click(function () {calcola("*");});
 
-function calcola(segno){
-    var n1 =parseInt($("#addendo1").value());
-    var n2 =parseInt($("#addendo2").value());
+  function calcola(segno) {
+    var n1 = parseInt($("#num1").val());
+    var n2 = parseInt($("#num2").val());
     var risultato;
-
-    switch(segno){
-        case "+" :
-            risultato=n1+n2;
-            break;
-        case "-" :
-            risultato=n1-n2;
-            break;
-        case "/" :
-            risultato=n1/n2;
-            break;
-        case "*" :
-            risultato=n1*n2;
-            break;
+    switch (segno) {
+      case "+":
+        risultato = n1 + n2;
+        break;
+      case "-":
+        risultato = n1 - n2;
+        break;
+      case "/":
+        risultato = n1 / n2;
+        break;
+      case "*":
+        risultato = n1 * n2;
+        break;
     }
     $("#span1").html(risultato);
-}
+ } 
