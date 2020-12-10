0983cdb18e907616d38d1b13c3df31d4fe19fc20
MicheleTheTruth <verita.michele@studenti.salesianisesto.it>
2020-12-10 17:45:00 +0100

    row cancellation

--- Es3.js
+++ Es3.js
@@ -3,11 +3,13 @@ $("#btn2").click(function () {calcola("-");});
 $("#btn3").click(function () {calcola("/");});
 $("#btn4").click(function () {calcola("*");});
 
+var cont=0;
+
   function calcola(segno) {
     var n1 = parseInt($("#num1").val());
     var n2 = parseInt($("#num2").val());
     var risultato;
-    var cont=1;
+  
     switch (segno) {
       case "+":
         risultato = n1 + n2;
@@ -22,7 +24,9 @@ $("#btn4").click(function () {calcola("*");});
         risultato = n1 * n2;
         break;
     }
+    var contS=cont.toString();
     $("#span1").html(risultato);
-    $("#tabella").append("<tr id='riga'><td>"+n1+"</td><td>"+segno+"</td><td>"+n2+"</td><td>=</td><td>"+risultato+"</td>"+"<td><button id='antani'>Cancella</button></td></tr>");
-    $("#antani").click(function(){$("#riga").remove();});
+    $("#tabella").append("<tr id="+contS+"><td>"+n1+"</td><td>"+segno+"</td><td>"+n2+"</td><td>=</td><td>"+risultato+"</td>"+"<td><button id="+contS+">Cancella</button></td></tr>");
+    $("#"+contS).click(function(){$("#"+contS).remove();});
+    cont++;
  } 
